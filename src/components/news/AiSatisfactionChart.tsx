635678f64c9f424bb5db69a057337d27aa5cc8ba
thanh thanh <thanhthanhai08@gmail.com>
2025-08-27 04:45:13 +0000

    Giảm padding của các cột so với khung ngoài cùng xuống để giãn biểu đồ r

--- src/components/news/AiSatisfactionChart.tsx
+++ src/components/news/AiSatisfactionChart.tsx
@@ -48,7 +48,7 @@ export function AiSatisfactionChart() {
         <ResponsiveContainer width="100%" height={400}>
           <BarChart
             data={data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 20 }}
+            margin={{ top: 5, right: 0, left: 0, bottom: 20 }}
             barGap={20} // Add gap between bars
             barSize={60} // Reduce bar size
           >
