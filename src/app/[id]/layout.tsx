84ee6a180fe9b07f33c1746b193c182c3860dc99
thanh thanh <thanhthanhai08@gmail.com>
2026-02-10 09:13:53 +0000

    bỏ dòng export const dynamic = 'force-dynamic'; ở Tôi sẽ cập nhật cả hai

--- src/app/[id]/layout.tsx
+++ src/app/[id]/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from "next";
 import { db } from "@/lib/firebase";
 import { doc, getDoc } from "firebase/firestore";
 
-export const dynamic = 'force-dynamic';
-
 type Props = {
   children: React.ReactNode;
   params: { id: string };
