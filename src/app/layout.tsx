4c7ccf908458ed68984a419ec4bea567c1159bd8
thanh thanh <thanhthanhai08@gmail.com>
2026-02-25 08:48:28 +0000

    Bỏ // trong phần   // { url: '/favicon.svg', type: 'image/svg+xml' }, //

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      // { url: '/favicon.svg', type: 'image/svg+xml' }, // Thêm favicon SVG chuẩn SEO
+      { url: '/favicon.svg', type: 'image/svg+xml' }, // Thêm favicon SVG chuẩn SEO
       // { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' }, 
       { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
